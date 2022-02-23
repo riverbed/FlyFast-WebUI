95fbdc7d8117775c283cdaa6e608db1d5e8c5dd1
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-02-23 16:27:24 -0500

    Added more fields for search bar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,11 +1,31 @@
 import { useState } from 'react';
-import { Autocomplete, Group, Button, Paper, LoadingOverlay } from '@mantine/core';
+import { Autocomplete, Group, Button, Paper, LoadingOverlay, NativeSelect } from '@mantine/core';
+import { DateRangePicker, DatePicker } from '@mantine/dates';
+
 import airports from './Airports.json';
 
 const Searchbar = ({ dataChange }) => {
+  const today = new Date();
+
   const [overlayShow, setOverlayShow] = useState(false);
   const [from, setFrom] = useState('');
   const [to, setTo] = useState('');
+  const [trip, setTrip] = useState('Round Trip');
+  const [seat, setSeat] = useState('Economy');
+  const [roundTripDate, setRoundTripDate] = useState([new Date(today.getTime() + (7 * 24 * 60 * 60 * 1000)), new Date(today.getTime() + (14 * 24 * 60 * 60 * 1000))]);
+  const [oneWayDate, setOneWayDate] = useState(new Date(today.getTime() + (7 * 24 * 60 * 60 * 1000)));
+
+  const tripTypes = [
+    { value: 'One Way', label: 'One Way' },
+    { value: 'Round Trip', label: 'Round Trip' }
+  ];
+
+  const seatTypes = [
+    { value: 'Economy', label: 'Economy' },
+    { value: 'Premium Economy', label: 'Premium Economy' },
+    { value: 'Business', label: 'Business' },
+    { value: 'First', label: 'First' },
+  ];
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -26,7 +46,22 @@ const Searchbar = ({ dataChange }) => {
     >
       <form onSubmit={handleSubmit}>
         <LoadingOverlay visible={overlayShow} />
-        <Group grow>
+        <Group my="sm">
+          <NativeSelect
+            required
+            data={tripTypes}
+            value={trip}
+            onChange={(e) => setTrip(e.currentTarget.value)}
+          />
+
+          <NativeSelect
+            required
+            data={seatTypes}
+            value={seat}
+            onChange={(e) => setSeat(e.currentTarget.value)}
+          />
+        </Group>
+        <Group grow my="sm">
           <Autocomplete
             required
             placeholder="From?"
@@ -42,8 +77,26 @@ const Searchbar = ({ dataChange }) => {
             value={to}
             onChange={setTo}
           />
+          { trip === "Round Trip" &&
+            <DateRangePicker
+              placeholder="Pick Trip Range"
+              amountOfMonths={2}
+              firstDayOfWeek="sunday"
+              value={roundTripDate}
+              onChange={setRoundTripDate}
+            />
+          }
+          { trip === "One Way" &&
+            <DatePicker
+              placeholder="Pick Trip Date"
+              amountOfMonths={1}
+              firstDayOfWeek="sunday"
+              value={oneWayDate}
+              onChange={setOneWayDate}
+            />
+          }
         </Group>
-        <Group mt="xl">
+        <Group my="sm" position="center">
           <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }} type="submit">
             Search
           </Button>
