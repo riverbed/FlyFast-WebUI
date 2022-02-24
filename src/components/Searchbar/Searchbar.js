4dcd93101cd7a2aa3e302cd01e16ec94dd3eff58
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-02-24 10:30:46 -0500

    Designed Searchbar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
-import { Autocomplete, Group, Button, Paper, LoadingOverlay, NativeSelect } from '@mantine/core';
+import { Autocomplete, Group, Button, Paper, NativeSelect, Grid } from '@mantine/core';
 import { DateRangePicker, DatePicker } from '@mantine/dates';
+import { FaPlaneArrival, FaPlaneDeparture, FaSearch } from "react-icons/fa";
+import { BsCalendarWeek } from "react-icons/bs";
+import { MdAirplanemodeActive, MdOutlineAirlineSeatReclineNormal } from "react-icons/md";
 
 import airports from './Airports.json';
 import { searchFlight } from '../../services/Flight';
@@ -8,7 +11,7 @@ import { searchFlight } from '../../services/Flight';
 const Searchbar = ({ dataChange }) => {
   const today = new Date();
 
-  const [overlayShow, setOverlayShow] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [from, setFrom] = useState('');
   const [to, setTo] = useState('');
   const [trip, setTrip] = useState('Round Trip');
@@ -28,12 +31,15 @@ const Searchbar = ({ dataChange }) => {
     { value: 'First', label: 'First' },
   ];
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
-    searchFlight(from, to)
+    setLoading(true);
+    await searchFlight(from, to)
       .then( result => {
         dataChange(JSON.stringify(result));
       })
+      .catch( error => console.error(error))
+    setLoading(false);
   }
 
   return (
@@ -42,10 +48,10 @@ const Searchbar = ({ dataChange }) => {
       shadow='sm'
     >
       <form onSubmit={handleSubmit}>
-        <LoadingOverlay visible={overlayShow} />
         <Group my="sm">
           <NativeSelect
             required
+            icon={<MdAirplanemodeActive />}
             data={tripTypes}
             value={trip}
             onChange={(e) => setTrip(e.currentTarget.value)}
@@ -53,48 +59,65 @@ const Searchbar = ({ dataChange }) => {
 
           <NativeSelect
             required
+            icon={<MdOutlineAirlineSeatReclineNormal />}
             data={seatTypes}
             value={seat}
             onChange={(e) => setSeat(e.currentTarget.value)}
           />
         </Group>
-        <Group grow my="sm">
-          <Autocomplete
-            required
-            placeholder="From?"
-            data={airports}
-            value={from}
-            onChange={setFrom}
-          />
+        <Grid my="sm" justify="center" align="center">
+          <Grid.Col md={8} sm={12}>
+            <Group grow>
+              <Autocomplete
+                required
+                icon={<FaPlaneDeparture />}
+                placeholder="From?"
+                data={airports}
+                value={from}
+                onChange={setFrom}
+              />
 
-          <Autocomplete
-            required
-            placeholder="To?"
-            data={airports}
-            value={to}
-            onChange={setTo}
-          />
-          { trip === "Round Trip" &&
-            <DateRangePicker
-              placeholder="Pick Trip Range"
-              amountOfMonths={2}
-              firstDayOfWeek="sunday"
-              value={roundTripDate}
-              onChange={setRoundTripDate}
-            />
-          }
-          { trip === "One Way" &&
-            <DatePicker
-              placeholder="Pick Trip Date"
-              amountOfMonths={1}
-              firstDayOfWeek="sunday"
-              value={oneWayDate}
-              onChange={setOneWayDate}
-            />
-          }
-        </Group>
+              <Autocomplete
+                required
+                icon={<FaPlaneArrival />}
+                placeholder="To?"
+                data={airports}
+                value={to}
+                onChange={setTo}
+              />
+            </Group>
+          </Grid.Col>
+          <Grid.Col md={4} sm={12}>
+            { trip === "Round Trip" &&
+              <DateRangePicker
+                icon={<BsCalendarWeek />}
+                placeholder="Pick Trip Range"
+                amountOfMonths={2}
+                firstDayOfWeek="sunday"
+                value={roundTripDate}
+                onChange={setRoundTripDate}
+              />
+            }
+            { trip === "One Way" &&
+              <DatePicker
+                icon={<BsCalendarWeek />}
+                placeholder="Pick Trip Date"
+                amountOfMonths={1}
+                firstDayOfWeek="sunday"
+                value={oneWayDate}
+                onChange={setOneWayDate}
+              />
+            }
+          </Grid.Col>
+        </Grid>
         <Group my="sm" position="center">
-          <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }} type="submit">
+          <Button
+            leftIcon={<FaSearch />}
+            variant="gradient"
+            gradient={{ from: 'indigo', to: 'cyan' }}
+            type="submit"
+            loading={loading}
+          >
             Search
           </Button>
         </Group>
