03628b7f3d4e76c4509a34510241f18ea632214f
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-03-25 10:51:18 -0400

    Setup Search Results as a Component

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { Stepper } from "@mantine/core";
+
+import TripCard from '../TripCard/TripCard';
+
+import { ImAirplane, ImHome, ImCart } from "react-icons/im";
+
+const SearchResults = ({ fromData, toData, results }) => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  return(
+    <Stepper active={activeStep} onStepClick={setActiveStep} breakpoint="xs">
+      <Stepper.Step label="Destination Flights" description={fromData + " - " + toData} icon={<ImAirplane />}>
+        {results[0].map((trip, index) => (
+          <TripCard
+            key={index}
+            from={trip.from}
+            to={trip.to}
+            flights={trip.flights}
+            departureTime={trip.departureTime}
+            arrivalTime={trip.arrivalTime}
+            fare={trip.fare}
+          />
+        ))}
+      </Stepper.Step>
+      { results.length === 2 && 
+        <Stepper.Step label="Return Flights" description={toData + " - " + fromData} icon={<ImHome />}>
+          {results[1].map((trip, index) => (
+            <TripCard
+              key={index}
+              from={trip.from}
+              to={trip.to}
+              flights={trip.flights}
+              departureTime={trip.departureTime}
+              arrivalTime={trip.arrivalTime}
+              fare={trip.fare}
+            />
+          ))}
+        </Stepper.Step>
+      }
+      <Stepper.Step label="Cart" description="Total Flights" icon={<ImCart />}>
+        {/* Implement Cart Functionality */}
+      </Stepper.Step>
+    </Stepper>
+  );
+}
+
+export default SearchResults;
--- src/pages/SearchFlight/SearchFlight.js
+++ src/pages/SearchFlight/SearchFlight.js
@@ -1,17 +1,15 @@
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-import { Grid, LoadingOverlay, Stepper } from "@mantine/core";
-import { ImAirplane, ImHome, ImCart } from "react-icons/im";
+import { Grid, LoadingOverlay } from "@mantine/core";
 
 import Search from '../../components/Search/Search';
-import TripCard from '../../components/TripCard/TripCard';
+import SearchResults from '../../components/SearchResults/SearchResults';
 import { searchFlight } from '../../services/Flight';
 
 const SearchFlight = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [overlayShow, setOverlayShow] = useState(false);
-  const [activeStep, setActiveStep] = useState(0);
   const [result, setResult] = useState([[]]);
   const testResult = [
     [
@@ -73,39 +71,11 @@ const SearchFlight = () => {
       </Grid.Col>
       <Grid.Col span={10}>
         <LoadingOverlay visible={overlayShow} />
-        <Stepper active={activeStep} onStepClick={setActiveStep} breakpoint="xs">
-          <Stepper.Step label="Destination Flights" description={searchParams.get('from') + " - " + searchParams.get('to')} icon={<ImAirplane />}>
-            {testResult[0].map((trip, index) => (
-              <TripCard
-                key={index}
-                from={trip.from}
-                to={trip.to}
-                flights={trip.flights}
-                departureTime={trip.departureTime}
-                arrivalTime={trip.arrivalTime}
-                fare={trip.fare}
-              />
-            ))}
-          </Stepper.Step>
-          { testResult.length === 2 && 
-            <Stepper.Step label="Return Flights" description={searchParams.get('to') + " - " + searchParams.get('from')} icon={<ImHome />}>
-              {testResult[1].map((trip, index) => (
-                <TripCard
-                  key={index}
-                  from={trip.from}
-                  to={trip.to}
-                  flights={trip.flights}
-                  departureTime={trip.departureTime}
-                  arrivalTime={trip.arrivalTime}
-                  fare={trip.fare}
-                />
-              ))}
-            </Stepper.Step>
-          }
-          <Stepper.Step label="Cart" description="Total Flights" icon={<ImCart />}>
-            {/* Implement Cart Functionality */}
-          </Stepper.Step>
-        </Stepper>
+        <SearchResults
+          fromData={searchParams.get('from')}
+          toData={searchParams.get('to')}
+          results={testResult}
+        />
       </Grid.Col>
     </Grid>
   );
