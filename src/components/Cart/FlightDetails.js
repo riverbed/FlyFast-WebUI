b07319eb5457a1e09d2244fe249de91feece593a
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-04-05 11:26:49 -0400

    Added Cards to Display Flights In Cart

--- src/components/Cart/FlightDetails.js
+++ src/components/Cart/FlightDetails.js
@@ -1,11 +1,13 @@
+import { Card } from '@mantine/core';
+
 import Flight from '../Flight/Flight';
 
 const FlightDetails = ({ flights }) => {
   return (
     flights.map((flight, index) => (
-      <div key={index}>
+      <Card radius="md" m="xs" withBorder key={index}>
         <Flight index={index} flight={flight} addCart={false} />
-      </div>
+      </Card>
     ))
   )
 }
